21c2e0ed3445adea1764880a7dc72933db28201d
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-10-04 16:42:41 -0300

    Handles HTTP user creation

---
+++ src/use-cases/register.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface UseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: UseCaseRequest) {
+    const passwordHash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findUserByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.createUser({
+      name,
+      email,
+      password_hash: passwordHash,
+    })
+  }
+}
