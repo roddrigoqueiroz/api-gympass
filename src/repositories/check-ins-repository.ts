285c7fd129e8a4f18e6e581fde11d02d5e657242
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-10-06 23:32:10 -0300

    Adding new methods to repositories

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,8 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   createCheckIn(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  findById(id: string): Promise<CheckIn | null>
+  countByUserId(userId: string): Promise<number>
+  saveCheckIn(checkIn: CheckIn): Promise<CheckIn>
 }
--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,13 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
+
+export interface SearchManyNearbyParams {
+  latitude: number
+  longitude: number
+}
 
 export interface GymsRepository {
   findGymById(id: string): Promise<Gym | null>
+  searchMany(query: string, page: number): Promise<Gym[]>
+  searchManyNearby(location: SearchManyNearbyParams): Promise<Gym[]>
+  createGym(data: Prisma.GymCreateInput): Promise<Gym>
 }
