6ea7cff2ccdf9d77cd25afec511700ac7b93a921
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-10-06 23:29:42 -0300

    Adding validate chek in use case and testfile

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,33 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from './utils/get-distance-between-coordinates'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
+
+interface CheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: CheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) throw new ResourceNotFoundError()
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.saveCheckIn(checkIn)
+
+    return { checkIn }
+  }
+}
